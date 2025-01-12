f35043a0afe503ab2f9d9975ae7bca1a97179f1a
Omkar Poojari <programmersdontsleep@gmail.com>
2025-01-12 21:25:27 +0530

    preferences api route and jwt verfication function completed

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const app = express();
 const port = 3000;
 const UserRegistration=require('./routes/UserRegistration');
 const UserLogin=require('./routes/UserLogin');
+const UserPreferences=require('./routes/UserPreferences');
 require('dotenv').config()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//verify jwttoken
 
 mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("Connected To MongoDb");
@@ -23,6 +25,7 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{
 
 app.use('/registerUser',UserRegistration);
 app.use('/userLogin',UserLogin);
+app.use('/preferences',UserPreferences);
 
 
 module.exports = app;
