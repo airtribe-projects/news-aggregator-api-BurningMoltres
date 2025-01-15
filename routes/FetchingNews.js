7f20285ab4c234f3cef5e4097ffcabb844b75fd9
Omkar Poojari <programmersdontsleep@gmail.com>
2025-01-16 02:53:01 +0530

    api speed

--- routes/FetchingNews.js
+++ routes/FetchingNews.js
@@ -3,8 +3,21 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
-const axios=require("axios");
+const axios = require("axios");
+const redis = require("redis");
+
 require("dotenv").config();
+
+//create redis client to connect to redis server
+let redisClient;
+
+(async () => {
+  redisClient = redis.createClient();
+  redisClient.on("error", (error) => {
+  console.log(error);
+  });
+  await redisClient.connect();
+})();
 
 //middleware function to verify jwt
 const verifyJWT = (req, res, next) => {
@@ -20,37 +33,44 @@ const verifyJWT = (req, res, next) => {
 
 router.use(verifyJWT);
 
-router.get("/",async (req,res,next)=>{
-    if(req.email.email.length<=0)
-    {
-        res.status(401).json("User is not authorised");
-    }
-    let email = req.email.email;
-    const userId = await User.findOne({ email });
-    const preferences=userId.preferences;
-    console.log(preferences);
+router.get("/", async (req, res, next) => {
+  if (req.email.email.length <= 0) {
+    res.status(401).json("User is not authorised");
+  }
+  let email = req.email.email;
+  const userId = await User.findOne({ email });
+  const preferences = userId.preferences;
+  console.log(preferences);
 
-    // Make separate Axios requests for each category in preferences
-  const promises = preferences.map(category =>
-    axios.get(`https://gnews.io/api/v4/top-headlines?category=${category}&apikey=${process.env.API_KEY}`)
+  //check if data is already cached
+  const cachedData = await redisClient.get("calculatedData");
+  if (cachedData) {
+    return res.status(200).json(JSON.parse(cachedData));
+  }
+  // Make separate Axios requests for each category in preferences
+  const promises = preferences.map((category) =>
+    axios.get(
+      `https://gnews.io/api/v4/top-headlines?category=${category}&apikey=${process.env.API_KEY}`
+    )
   );
 
-  try
-  {
+  try {
     //Wait for all requests to finish
-    const responses=await Promise.all(promises);
+    const responses = await Promise.all(promises);
 
-    //combine articles from all responses into single array
-    const allArticles=responses.flatMap(response => response.data.articles);
+    const allArticles = responses.flatMap((response) => response.data.articles);
+    await redisClient.set(
+      "calculatedData",
+      JSON.stringify(allArticles),
+      "EX",
+      3600
+    );
     console.log(allArticles);
     res.status(200).json(allArticles);
-
-  }
-  catch (error) {
+  } catch (error) {
     console.error(error);
     res.status(500).json("Internal Server Error"); // Handle errors gracefully
   }
+});
 
-})
-
-module.exports=router;
+module.exports = router;
